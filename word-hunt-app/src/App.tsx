582f37284807fe0d0b5b0f32277fc5ce6bc3e609
Levi Lutz <levi6900@icloud.com>
2025-09-01 21:49:42 -0400

    Flesh out rendering for word tile grid, no interactivity yet

--- word-hunt-app/src/App.tsx
+++ word-hunt-app/src/App.tsx
@@ -1,7 +1,47 @@
-import { Button } from "@mui/material";
+import WordHunt from "./WordHunt";
 
 function App() {
-	return <Button variant="contained">Hello World!</Button>;
+	return (
+		<WordHunt
+			width={135}
+			height={135}
+			grid={[
+				[
+					{ contents: "A", highlight: "none" },
+					{ contents: "B", highlight: "none" },
+					{ contents: "C", highlight: "none" },
+					{ contents: "D", highlight: "none" },
+				],
+				[
+					{ contents: "D", highlight: "hover" },
+					{ contents: "E", highlight: "selected" },
+					{ contents: "F", highlight: "new-word" },
+					{ contents: "G", highlight: "used-word" },
+				],
+				[
+					{ contents: "H", highlight: "hover" },
+					{ contents: "I", highlight: "selected" },
+					{ contents: "J", highlight: "new-word" },
+					{ contents: "K", highlight: "used-word" },
+				],
+				[
+					{ contents: "L", highlight: "none" },
+					{ contents: "M", highlight: "none" },
+					{ contents: "N", highlight: "none" },
+					{ contents: "O", highlight: "none" },
+				],
+			]}
+			path={[
+				{ x: 0, y: 0 },
+				{ x: 1, y: 1 },
+				{ x: 1, y: 2 },
+				{ x: 0, y: 3 },
+			]}
+			onPathStart={() => {}}
+			onPathFinish={() => {}}
+			onTileHover={() => {}}
+		/>
+	);
 }
 
 export default App;
