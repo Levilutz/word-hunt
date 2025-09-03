355651ec68a9c57cbc38012e40d213f2cf8ffb73
Levi Lutz <levi6900@icloud.com>
2025-09-03 18:53:23 -0400

    Add mask for beveled edge

--- word-hunt-app/src/WordHuntTile.tsx
+++ word-hunt-app/src/WordHuntTile.tsx
@@ -10,7 +10,7 @@ import {
 	Texture,
 	TilingSprite,
 } from "pixi.js";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import grass from "./assets/grass.png";
 import hardwood from "./assets/hardwood.jpg";
 
@@ -20,6 +20,7 @@ export type WordHuntTileProps = {
 
 export default function WordHuntTile({ contents }: WordHuntTileProps) {
 	useExtend({ Sprite, Graphics, Text, TilingSprite, Container });
+	const maskRef = useRef<Graphics>(null);
 	const [hardwoodTexture, setHardwoodTexture] = useState(Texture.EMPTY);
 	const [grassTexture, setGrassTexture] = useState(Texture.EMPTY);
 	const [buttonHover, setButtonHover] = useState(false);
@@ -49,14 +50,21 @@ export default function WordHuntTile({ contents }: WordHuntTileProps) {
 				y={9}
 				width={90}
 				height={90}
-				interactive
+				eventMode="static"
 				cursor="pointer"
 				onMouseEnter={() => setButtonHover(true)}
 				onMouseLeave={() => setButtonHover(false)}
 				onMouseDown={() => setButtonClick(true)}
 				onMouseUp={() => setButtonClick(false)}
 				alpha={buttonClick ? 0.4 : buttonHover ? 0.8 : 1}
+				mask={maskRef?.current}
 			>
+				<pixiGraphics
+					ref={maskRef}
+					draw={(graphics) => {
+						graphics.roundRect(0, 0, 90, 90, 9).fill({ color: 0xffffff });
+					}}
+				/>
 				<pixiSprite
 					x={0}
 					y={0}
