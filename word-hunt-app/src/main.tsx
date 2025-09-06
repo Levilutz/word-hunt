9b602b36b33dbca04219264269eddef735ba484e
Levi Lutz <levi6900@icloud.com>
2025-09-06 12:12:45 -0400

    Swap to vanilla pixi app with react component to manage lifecycle

--- word-hunt-app/src/main.tsx
+++ word-hunt-app/src/main.tsx
@@ -1,11 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
+import VanillaApp from "./VanillaApp.tsx";
 
 // biome-ignore lint/style/noNonNullAssertion: Hardcoded in index.html
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<App />
+		<VanillaApp />
 	</StrictMode>,
 );
