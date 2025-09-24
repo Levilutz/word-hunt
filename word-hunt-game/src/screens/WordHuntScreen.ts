117c80b5eb0b9612bfc8fa3f4f8c7b77d265d056
Levi Lutz <levi6900@icloud.com>
2025-09-24 18:23:09 -0400

    Overhaul WordHuntScreen to actually render tiles with optimized pointer

--- word-hunt-game/src/screens/WordHuntScreen.ts
+++ word-hunt-game/src/screens/WordHuntScreen.ts
@@ -9,6 +9,11 @@ import type { AppScreen } from "../Navigation";
 import type { AppState } from "../State";
 import {
   getTilePx,
+  gridSize,
+  pointAdd,
+  pointAdjacent,
+  pointFloor,
+  pointInList,
   pointScale,
   pointSub,
   thickRaster,
@@ -16,28 +21,67 @@ import {
 } from "../utils";
 import WordHuntTile from "../ui/WordHuntTile";
 
-export default class WordHuntScreen extends Container implements AppScreen {
-  private appState: AppState;
+// Constants
+const usedRatio = 0.75;
+const spaceRatio = 0.1;
 
-  private _w: number;
-  private _h: number;
+export default class WordHuntScreen extends Container implements AppScreen {
+  private readonly appState: AppState;
+
+  /** The size of the logical grid of tiles. Typically 4x4. */
+  private readonly _gridSize: PointData;
+
+  /** The width of the screen's area in Px. */
+  private _w: number = 200;
+  /** The height of the screen's area in Px. */
+  private _h: number = 200;
+
+  /** The width and height of a single tile in Px. */
+  private _tilePx: number = 40;
+  /** The space between any two tiles in Px. */
+  private _spacePx: number = 4;
+  /** The pixel coordinates of the top-left corner of the first grid tile. */
+  private _gridRenderStart: PointData = { x: 14, y: 14 };
 
   private readonly _hitContainer = new Container();
   private readonly _hitArea = new Rectangle();
 
+  private readonly tiles: (WordHuntTile | null)[][];
+
   private readonly _graphics = new Graphics();
   private pointerDown = false;
   private lastPos: PointData = { x: 0, y: 0 };
-  private pointDownPos: PointData | undefined = undefined;
+
+  private curPath: PointData[] = [];
 
   constructor(appState: AppState, w: number, h: number) {
     super();
 
     this.appState = appState;
+    this._gridSize = gridSize(this.appState.grid);
+
     this._w = w;
     this._h = h;
+    this._hitArea.width = w;
+    this._hitArea.height = h;
+    this.updateCalculatedSizes();
 
-    this.addChild(this._graphics);
+    this.tiles = this.appState.grid.map((row, y) =>
+      row.map((value, x) => {
+        if (value === null) {
+          return null;
+        }
+        const tilePos = this.getTilePos({ x, y });
+        const tile = new WordHuntTile(
+          tilePos.x,
+          tilePos.y,
+          this._tilePx,
+          value,
+        );
+        this.addChild(tile);
+        return tile;
+      }),
+    );
 
     this._hitContainer.interactive = true;
     this._hitContainer.hitArea = this._hitArea;
@@ -47,131 +91,128 @@ export default class WordHuntScreen extends Container implements AppScreen {
     this._hitContainer.on("pointerdown", this.handlePointerDown.bind(this));
     this._hitContainer.on("pointermove", this.handlePointerMove.bind(this));
 
-    this.addChild(new WordHuntTile(50, 50, 100, 100, "A"));
-
-    this.renderTiles();
+    this.addChild(this._graphics);
   }
 
   resize(w: number, h: number) {
+    // Update w and h top-level fields, and calculated fields
     this._w = w;
     this._h = h;
     this._hitArea.width = w;
     this._hitArea.height = h;
-    this.renderTiles();
+    this.updateCalculatedSizes();
+
+    // Update tile positions
+    this.tiles.forEach((row, y) => {
+      row.forEach((tile, x) => {
+        if (tile === null) {
+          return;
+        }
+        const tilePos = this.getTilePos({ x, y });
+        tile.setBounds(tilePos.x, tilePos.y, this._tilePx);
+      });
+    });
   }
 
-  private handlePointerUp(_event: FederatedPointerEvent) {
+  /** Update the sizes and positions of things dependent on width & height. */
+  private updateCalculatedSizes() {
+    this._tilePx = Math.min(
+      getTilePx(this._w, usedRatio, spaceRatio, this._gridSize.x),
+      getTilePx(this._h, usedRatio, spaceRatio, this._gridSize.y),
+    );
+    this._spacePx = this._tilePx * spaceRatio;
+    const usedW =
+      this._gridSize.x * this._tilePx + (this._gridSize.x - 1) * this._spacePx;
+    const usedH =
+      this._gridSize.y * this._tilePx + (this._gridSize.y - 1) * this._spacePx;
+    this._gridRenderStart = {
+      x: (this._w - usedW) / 2,
+      y: (this._h - usedH) / 2,
+    };
+  }
+
+  private getTilePos(coords: PointData): PointData {
+    return pointAdd(
+      pointScale(coords, this._tilePx + this._spacePx),
+      this._gridRenderStart,
+    );
+  }
+
+  private handlePointerUp() {
     this.pointerDown = false;
+    if (this.curPath.length === 0) {
+      return;
+    }
+    console.log(
+      this.curPath.map(({ x, y }) => this.appState.grid[y][x]!).join(""),
+    );
+    this.curPath = [];
+    this.tiles.forEach((row) => {
+      row.forEach((tile) => {
+        if (tile === null) {
+          return;
+        }
+        tile.setPressed(false);
+      });
+    });
   }
 
   private handlePointerDown({ global }: FederatedPointerEvent) {
     const { x, y } = global;
     this.pointerDown = true;
-    this.pointDownPos = { x, y };
+    const tilePos = pointFloor(this.scaledForTiles({ x, y }));
+    if (this.tileExists(tilePos)) {
+      this.curPath = [tilePos];
+      this.tiles[tilePos.y][tilePos.x]?.setPressed(true);
+    } else {
+      this.curPath = [];
+    }
   }
 
-  private handlePointerMove({ global }: FederatedPointerEvent) {
+  private handlePointerMove({ global, buttons }: FederatedPointerEvent) {
     const { x, y } = global;
-    this.lastPos = { x, y };
-    this.renderTiles();
-  }
-
-  /** How many px in do we start rendering the grid. */
-  private gridRenderStart(): PointData {
-    const { w, h } = this.gridSize();
-    const { tilePx, spacePx } = this.tileSpacePx();
-    const usedW = w * tilePx + (w - 1) * spacePx;
-    const usedH = h * tilePx + (h - 1) * spacePx;
-    return { x: (this._w - usedW) / 2, y: (this._h - usedH) / 2 };
-  }
-
-  /** What tile & space Px should we target. */
-  private tileSpacePx(): { tilePx: number; spacePx: number } {
-    const { w, h } = this.gridSize();
-    // Constants
-    const usedRatio = 0.75;
-    const spaceRatio = 0.1;
-    // Tilepx is min of what we need for width vs height
-    const tilePx = Math.min(
-      getTilePx(this._w, usedRatio, spaceRatio, w),
-      getTilePx(this._h, usedRatio, spaceRatio, h),
-    );
-    return { tilePx, spacePx: tilePx * spaceRatio };
-  }
-
-  /** What is the size of the logical word hunt grid. */
-  private gridSize(): { w: number; h: number } {
-    return {
-      w:
-        this.appState.grid.length > 0
-          ? Math.max(...this.appState.grid.map((row) => row.length))
-          : 0,
-      h: this.appState.grid.length,
-    };
-  }
-
-  private renderTiles() {
-    const { tilePx, spacePx } = this.tileSpacePx();
-    const gridRenderStart = this.gridRenderStart();
-    this._graphics.clear();
-    let highlight1: PointData[] = [];
-    let highlight2: PointData[] = [];
-    if (this.pointDownPos !== undefined) {
-      highlight1 = thickRaster(
-        pointScale(
-          pointSub(this.pointDownPos, gridRenderStart),
-          1 / (tilePx + spacePx),
-        ),
-        pointScale(
-          pointSub(this.lastPos, gridRenderStart),
-          1 / (tilePx + spacePx),
-        ),
+    if (buttons === 0) {
+      this.handlePointerUp();
+    } else if (this.pointerDown && this.curPath.length > 0) {
+      const affected = thickRasterCircles(
+        this.scaledForTiles(this.lastPos),
+        this.scaledForTiles({ x, y }),
       );
-      highlight2 = thickRasterCircles(
-        pointScale(
-          pointSub(this.pointDownPos, gridRenderStart),
-          1 / (tilePx + spacePx),
-        ),
-        pointScale(
-          pointSub(this.lastPos, gridRenderStart),
-          1 / (tilePx + spacePx),
-        ),
-      );
-    }
-    console.log(highlight1);
-    this.appState.grid.forEach((row, y) => {
-      row.forEach((contents, x) => {
-        if (contents === null) {
-          return;
+      for (const tilePos of affected) {
+        if (pointInList(this.curPath, tilePos)) {
+        } else if (
+          pointAdjacent(this.curPath[this.curPath.length - 1], tilePos) &&
+          this.tileExists(tilePos)
+        ) {
+          this.curPath.push(tilePos);
+          this.tiles[tilePos.y][tilePos.x]?.setPressed(true);
+        } else {
+          break;
         }
-        const x1 = (tilePx + spacePx) * x + gridRenderStart.x;
-        const y1 = (tilePx + spacePx) * y + gridRenderStart.y;
-        // const { x: lastX, y: lastY } = this.lastPos;
-        // const hovered =
-        //   lastX >= x1 &&
-        //   lastX < x1 + tilePx &&
-        //   lastY >= y1 &&
-        //   lastY < y1 + tilePx;
-
-        const highlighted1 = highlight1.some(
-          (pos) => pos.x === x && pos.y === y,
-        );
-        const highlighted2 = highlight2.some(
-          (pos) => pos.x === x && pos.y === y,
-        );
-        this._graphics.rect(x1, y1, tilePx, tilePx).fill({
-          color: highlighted2 ? 0xff0000 : highlighted1 ? 0x0000ff : 0x00ff00,
-        });
-        this._graphics
-          .circle(x1 + tilePx / 2, y1 + tilePx / 2, (tilePx + spacePx) / 2)
-          .stroke({ width: 1, color: 0x000000 });
-      });
-    });
-    if (this.pointDownPos !== undefined) {
-      this._graphics
-        .moveTo(this.pointDownPos.x, this.pointDownPos.y)
-        .lineTo(this.lastPos.x, this.lastPos.y)
-        .stroke({ width: 2, color: 0x00ff00 });
+      }
     }
+    this.lastPos = { x, y };
+  }
+
+  /** Scale the given pixel coordinates to logical coords in tile hit areas.
+   *
+   * e.g. for 100px tiles starting at (50, 50) a pos of (255, 345) would map to (2.05, 2.95)
+   */
+  private scaledForTiles(p: PointData): PointData {
+    return pointScale(
+      pointSub(
+        p,
+        pointSub(this._gridRenderStart, {
+          x: this._spacePx * 0.5,
+          y: this._spacePx * 0.5,
+        }),
+      ),
+      1 / (this._tilePx + this._spacePx),
+    );
+  }
+
+  /** Check if a tile at the given coordinates exists. */
+  private tileExists(p: PointData) {
+    return this.tiles?.[p.y]?.[p.x] != null;
   }
 }
