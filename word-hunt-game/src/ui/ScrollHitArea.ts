1acbcc67e15f617bcb8a224d02c1ecbdbed646c2
Levi Lutz <levi6900@icloud.com>
2025-09-27 17:02:40 -0400

    Scroll Hit Area is all global events

--- word-hunt-game/src/ui/ScrollHitArea.ts
+++ word-hunt-game/src/ui/ScrollHitArea.ts
@@ -1,17 +1,19 @@
 import {
   Container,
   type DestroyOptions,
-  type FederatedPointerEvent,
-  type FederatedWheelEvent,
   type PointData,
   Rectangle,
 } from "pixi.js";
 
 export default class ScrollHitArea extends Container {
+  private _w: number;
+  private _h: number;
   private _pressed = false;
   private _lastPos: PointData = { x: 0, y: 0 };
   private readonly _boundHandlePointerUp: (event: PointerEvent) => void;
   private readonly _boundHandlePointerMove: (event: PointerEvent) => void;
+  private readonly _boundHandlePointerDown: (event: PointerEvent) => void;
+  private readonly _boundHandleWheel: (event: WheelEvent) => void;
   private readonly _onScroll?: (px: number) => void;
 
   constructor(
@@ -21,26 +23,35 @@ export default class ScrollHitArea extends Container {
     height: number,
     onScroll?: (px: number) => void,
   ) {
-    super({ x, y, interactive: true });
+    super({ x, y });
+    this._w = width;
+    this._h = height;
     this._onScroll = onScroll;
     this.hitArea = new Rectangle(0, 0, width, height);
     this._boundHandlePointerUp = this.handlePointerUp.bind(this);
     document.addEventListener("pointerup", this._boundHandlePointerUp);
+    this._boundHandlePointerDown = this.handlePointerDown.bind(this);
+    document.addEventListener("pointerdown", this._boundHandlePointerDown);
     this._boundHandlePointerMove = this.handlePointerMove.bind(this);
     document.addEventListener("pointermove", this._boundHandlePointerMove);
-    this.on("pointerdown", this.handlePointerDown.bind(this));
+    this._boundHandleWheel = this.handleWheel.bind(this);
+    document.addEventListener("wheel", this._boundHandleWheel);
     this.on("wheel", this.handleWheel.bind(this));
   }
 
   resize(x: number, y: number, w: number, h: number) {
     this.x = x;
     this.y = y;
+    this._w = w;
+    this._h = h;
     this.hitArea = new Rectangle(0, 0, w, h);
   }
 
   destroy(options?: DestroyOptions) {
     document.removeEventListener("pointerup", this._boundHandlePointerUp);
+    document.removeEventListener("pointerdown", this._boundHandlePointerDown);
     document.removeEventListener("pointermove", this._boundHandlePointerMove);
+    document.removeEventListener("wheel", this._boundHandleWheel);
     super.destroy(options);
   }
 
@@ -48,8 +59,10 @@ export default class ScrollHitArea extends Container {
     this._pressed = false;
   }
 
-  private handlePointerDown({ global }: FederatedPointerEvent) {
-    const { x, y } = global;
+  private handlePointerDown({ x, y }: PointerEvent) {
+    if (!this.contains(x, y)) {
+      return;
+    }
     this._pressed = true;
     this._lastPos = { x, y };
   }
@@ -62,7 +75,14 @@ export default class ScrollHitArea extends Container {
     this._lastPos = { x, y };
   }
 
-  private handleWheel({ deltaY }: FederatedWheelEvent) {
+  private handleWheel({ x, y, deltaY }: WheelEvent) {
+    if (!this.contains(x, y)) {
+      return;
+    }
     this._onScroll?.(deltaY);
+  }
+
+  private contains(x: number, y: number) {
+    return new Rectangle(this.x, this.y, this._w, this._h).contains(x, y);
   }
 }
