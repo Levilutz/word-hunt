b6edbbe445e2e137dea52f96d25a05c111a41f4c
Levi Lutz <levi6900@icloud.com>
2025-09-28 17:02:21 -0400

    Add postgres and psycopg, example endpoint to query tables

--- word-hunt-service/main.py
+++ word-hunt-service/main.py
@@ -1,5 +1,9 @@
+import os
+
 from fastapi import FastAPI
 from pydantic import BaseModel
+import psycopg
+from psycopg.rows import class_row
 
 app = FastAPI()
 
@@ -9,6 +13,27 @@ class RootResp(BaseModel):
     baz: int
 
 
+class PgTable(BaseModel):
+    schemaname: str
+    tablename: str
+    tableowner: str
+    tablespace: str | None
+    hasindexes: bool
+    hasrules: bool
+    hastriggers: bool
+    rowsecurity: bool
+
+
 @app.get("/")
 async def root() -> RootResp:
     return RootResp(foo="bar", baz=125)
+
+
+@app.get("/test")
+async def test() -> list[PgTable]:
+    async with await psycopg.AsyncConnection.connect(
+        os.getenv("POSTGRES_URL", "")
+    ) as conn:
+        async with conn.cursor(row_factory=class_row(PgTable)) as cur:
+            await cur.execute("SELECT * FROM pg_catalog.pg_tables")
+            return await cur.fetchall()
