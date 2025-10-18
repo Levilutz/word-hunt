65982c8a32f4794bf9971738b7d7f7311c621927
Levi Lutz <levi6900@icloud.com>
2025-10-18 13:11:35 -0400

    Versus-first table schema

--- word-hunt-service/migrations/20250928212705_initial.down.sql
+++ word-hunt-service/migrations/20250928212705_initial.down.sql
@@ -1,1 +1,7 @@
-RAISE EXCEPTION "Cannot down";
+BEGIN;
+
+DROP TABLE IF EXISTS games_match_queue;
+DROP TABLE IF EXISTS versus_games;
+DROP TABLE IF EXISTS veresus_game_submitted_words;
+
+COMMIT;
--- word-hunt-service/migrations/20250928212705_initial.up.sql
+++ word-hunt-service/migrations/20250928212705_initial.up.sql
@@ -1,34 +1,33 @@
 BEGIN;
 
-CREATE TYPE game_mode AS ENUM ('versus', 'solo', 'solve');
-
-CREATE TABLE IF NOT EXISTS games(
-    id UUID PRIMARY KEY,
-    created_at TIMESTAMP NOT NULL,
-    creator_id UUID NOT NULL,
-    competitor_id UUID,
-    game_mode game_mode NOT NULL,
-    grid JSONB NOT NULL,
-    start_time TIMESTAMP,
-    end_time TIMESTAMP
+CREATE TABLE IF NOT EXISTS games_match_queue(
+    session_id UUID PRIMARY KEY,
+    join_time TIMESTAMP NOT NULL,
+    game_id UUID
 );
 
-CREATE INDEX games_creator_id ON games (creator_id);
-CREATE INDEX games_competitor_id ON games (competitor_id);
+CREATE INDEX games_match_queue_last_ping_time_join_time ON games_match_queue (last_ping_time, join_time);
 
-CREATE INDEX games_awaiting_competitor
-ON games (created_at)
-WHERE competitor_id IS NULL
-AND game_mode = 'versus';
-
-CREATE TABLE IF NOT EXISTS game_submitted_words(
+CREATE TABLE IF NOT EXISTS versus_games(
     id UUID PRIMARY KEY,
-    game_id UUID NOT NULL REFERENCES games(id) ON DELETE CASCADE,
-    submitter_id UUID NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    session_id_a UUID NOT NULL,
+    session_id_b UUID NOT NULL,
+    grid JSONB NOT NULL
+);
+
+CREATE INDEX versus_games_created_at ON versus_games (created_at);
+CREATE INDEX versus_games_session_id_a ON versus_games (session_id_a);
+CREATE INDEX versus_games_session_id_b ON versus_games (session_id_b);
+
+CREATE TABLE IF NOT EXISTS versus_game_submitted_words(
+    id UUID PRIMARY KEY,
+    game_id UUID NOT NULL REFERENCES versus_games(id) ON DELETE CASCADE,
+    session_id UUID NOT NULL,
     tile_path JSONB NOT NULL,
     word VARCHAR NOT NULL
 );
 
-CREATE INDEX game_submitted_words_game_id ON game_submitted_words(game_id);
+CREATE INDEX versus_game_submitted_words_submitted_words_game_id ON versus_game_submitted_words(game_id);
 
 COMMIT;
