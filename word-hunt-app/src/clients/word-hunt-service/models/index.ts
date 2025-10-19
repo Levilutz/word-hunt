d1a279483ca1d2008452b8d32002f71f8a8fe24e
Levi Lutz <levi6900@icloud.com>
2025-10-18 22:25:17 -0400

    Remove old version of generated client

--- word-hunt-app/src/clients/word-hunt-service/models/index.ts
+++
@@ -1,8 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export * from './CreateGameReq';
-export * from './GameMode';
-export * from './GetGameResp';
-export * from './HTTPValidationError';
-export * from './ValidationError';
-export * from './ValidationErrorLocInner';
