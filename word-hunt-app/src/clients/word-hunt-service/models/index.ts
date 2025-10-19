5666a0d96b3d88af5f4bfecd36501d1a93295915
Levi Lutz <levi6900@icloud.com>
2025-10-18 22:25:50 -0400

    Generate new word-hunt-service client

---
+++ word-hunt-app/src/clients/word-hunt-service/models/index.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './GetGameResp';
+export * from './GetGameRespPlayer';
+export * from './HTTPValidationError';
+export * from './Point';
+export * from './PostMatchResp';
+export * from './SubmitWordsReq';
+export * from './ValidationError';
+export * from './ValidationErrorLocInner';
